c4f2751cf1078f8eb0c8f50f99eb77f2197b833c
Marco Cinus <marcocinus@MacBook-Air-di-Marco.local>
2026-07-15 17:49:18 +0200

    update sidebar

--- src/app/components/shell/shell.ts
+++ src/app/components/shell/shell.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
-import { RouterOutlet, RouterLinkWithHref, RouterLinkActive } from '@angular/router';
+import { Component, signal } from '@angular/core';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-shell',
-  imports: [RouterOutlet, RouterLinkWithHref, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, RouterOutlet],
   templateUrl: './shell.html',
   styleUrl: './shell.css',
 })
-export class Shell {}
+export class Shell {
+  sidebarOpen = signal(false);
+
+  toggleSidebar(): void {
+    this.sidebarOpen.update((open) => !open);
+  }
+}
